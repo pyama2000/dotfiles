3353e81bb5f05c46a51cb30c2f31c00d4192cc6a
T.YAMASHITA <8256tako@gmail.com>
2019-07-19 18:11:55 +0900

    :sparkles: Create for setup dev tools

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+set -eu -o pipefail
+trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
+
+wget --no-check-certificate https://github.com/pyama2000/dotfiles/archive/master.tar.gz
+tar xzvf master.tar.gz
+mv ~/dotfiles-master ~/dotfiles
+bash ~/dotfiles/setup.sh
+cd dotfiles
+git init
+git remote add git@github.com:pyama2000/dotfiles.git

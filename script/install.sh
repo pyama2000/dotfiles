8a51a0f1a32def3d42053dc1aa98738675867a69
T.YAMASHITA <8256tako@gmail.com>
2020-03-10 16:13:41 +0900

    :sparkles: Use GitHub Actions

---
+++ script/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ue
+
+if [ $(uname) == "Darwin" ]; then
+  OS="macOS"
+elif [ $(expr substr $(uname -s) 1 5) == "Linux" ]; then
+  OS="Linux"
+else
+  echo "Your platform ($(uname -a)) is not suppported."
+  exit 1
+fi
+
+# if [ $OS == "macOS" ]; then
+#   xcode-select --install
+# fi
+
+if [ $OS == "Linux" ]; then
+  sudo apt update -y
+  sudo apt install -y curl build-essential software-properties-common
+fi
+
+curl --proto '=https' --tlsv1.2 https://sh.rustup.sh | sh -s -- -y
+source $HOME/.cargo/env
+
+cargo install cargo-make

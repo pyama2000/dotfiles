5529f9e298cfb4ef8028882874229809f551a99f
T.YAMASHITA <8256tako@gmail.com>
2020-04-22 14:22:11 +0900

    :+1: Add zoxide config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -41,6 +41,11 @@ set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 eval (starship init fish)
 
 ##############################
+# zoxide   ###################
+##############################
+zoxide init fish | source
+
+##############################
 # Alias ######################
 ##############################
 ## ghq

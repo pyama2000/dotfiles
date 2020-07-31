2efcf0e87666c14e307ce02d3ac1cf17b9287817
T.YAMASHITA <8256tako@gmail.com>
2020-07-31 10:47:49 +0900

    :bug: Fix ghrm.fish

--- .config/fish/functions/ghrm.fish
+++ .config/fish/functions/ghrm.fish
@@ -2,7 +2,7 @@ function ghrm
   set DIRECTORY_NAME $argv[1]
 
   if [ -n "$DIRECTORY_NAME" ]
-    rm -rf (ghq list --full-path --exact $DIRECTORY_NAME
+    rm -rf (ghq list --full-path --exact $DIRECTORY_NAME)
   else
     rm -rf (ghq list --full-path | fzf)
   end

7cfef79d6b10d0533f4ba61ab1b58f7674eecc08
pyama2000 <8256tako@gmail.com>
2022-05-28 19:15:24 +0900

    Remove unnecessary comment

--- nvim/plugins/nvim-treesitter.lua
+++ nvim/plugins/nvim-treesitter.lua
@@ -1,18 +1,3 @@
--- require'nvim-treesitter.configs'.setup {
---   ensure_installed = "maintained",
---   highlight = {
---     enable = true,
---     disable = {
---       'lua',
---       'ruby',
---       'c_sharp',
---       'dockerfile',
---     }
---   },
---   indent = {
---     enable = false,
---   }
--- }
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "maintained",
 	highlight = {

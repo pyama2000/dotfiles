9ce42a383f5f83e417c5509d5ccab94b12c15142
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2022-05-28 19:15:36 +0900

    Use lualine.nvim (#117)
    * Use lualine.nvim with default config
    
    * Fix lualine.nvim config
    
    * Format code
    
    * Remove unnecessary comment

--- nvim/plugins/nvim-treesitter.lua
+++ nvim/plugins/nvim-treesitter.lua
@@ -1,21 +1,6 @@
--- require'nvim-treesitter.configs'.setup {
---   ensure_installed = "maintained",
---   highlight = {
---     enable = true,
---     disable = {
---       'lua',
---       'ruby',
---       'c_sharp',
---       'dockerfile',
---     }
---   },
---   indent = {
---     enable = false,
---   }
--- }
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained",
-  highlight = {
-    enable = true,
-  }
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "maintained",
+	highlight = {
+		enable = true,
+	},
+})

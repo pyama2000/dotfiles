f1dfef8398dd35dd447d872399a6d9f34fd95b82
pyama2000 <8256tako@gmail.com>
2022-10-03 23:57:07 +0900

    feat: Override coc hl-groups

---
+++ nvim/plugins/kanagawa.lua
@@ -0,0 +1,8 @@
+local default_colors = require("kanagawa.colors").setup()
+
+local overrides = {
+    CocFloating = { fg = default_colors.fugiWhite, bg = default_colors.waveBlue1 },
+    CocMenuSel = { fg = default_colors.fugiWhite, bg = default_colors.waveBlue2, bold = true },
+}
+
+require'kanagawa'.setup({ overrides = overrides })

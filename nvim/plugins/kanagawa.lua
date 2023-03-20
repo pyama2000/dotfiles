8e1832a218e8168203ea117a64e354235be6a1a0
pyama2000 <8256tako@gmail.com>
2023-03-20 11:23:14 +0900

    fix: fix kanagawa

--- nvim/plugins/kanagawa.lua
+++ nvim/plugins/kanagawa.lua
@@ -1,8 +1,8 @@
-local default_colors = require("kanagawa.colors").setup()
-
-local overrides = {
-	CocFloating = { fg = default_colors.fugiWhite, bg = default_colors.waveBlue1 },
-	CocMenuSel = { fg = default_colors.fugiWhite, bg = default_colors.waveBlue2, bold = true },
-}
-
-require("kanagawa").setup({ overrides = overrides })
+require('kanagawa').setup({
+  overrides = function(colors)
+    return {
+      CocFloating = { fg = colors.palette.fugiWhite, bg = colors.palette.waveBlue1 },
+      CocMenuSel = { fg = colors.palette.fugiWhite, bg = colors.palette.waveBlue2, bold = true },
+    }
+  end,
+})

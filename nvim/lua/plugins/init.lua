a4c59d6abac1ef1ff425d0f76fdb8625c7530d5e
pyama2000 <8256tako@gmail.com>
2023-05-28 11:22:55 +0900

    fix: fix Telescope config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,10 +1,5 @@
 return {
   {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    cmd = "Telescope",
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
   },
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,27 +1,54 @@
-local ok, telescope = pcall(require, "telescope")
-if not ok then
-  return
-end
-
-local actions = require("telescope.actions")
-local builtin = require("telescope.builtin")
-
-require("telescope").setup({
-  defaults = {
-    mappings = {
-      ["n"] = {
-        ["q"] = actions.close,
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-file-browser.nvim",
+        config = function()
+          require("telescope").load_extension("file_browser")
+        end,
       },
     },
-  },
-  extensions = {
-    file_browser = {},
-  },
-})
-require("telescope").load_extension("file_browser")
+    event = "VeryLazy",
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
 
-vim.keymap.set("n", "<C-f>b", telescope.extensions.file_browser.file_browser, { noremap = true, silent = true })
-vim.keymap.set("n", "<C-d>f", builtin.find_files, { noremap = true, silent = true })
-vim.keymap.set("n", "<C-d>b", builtin.buffers, { noremap = true, silent = true })
+      telescope.setup({
+        defaults = {
+          mappings = {
+            n = {
+              ["<ECS>"] = actions.close,
+              ["q"] = actions.close,
+            },
+            i = {
+              ["<ECS>"] = actions.close,
+            },
+          },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+          },
+          file_ignore_patterns = {
+            ".git/",
+          },
+        },
+      })
 
-return {}
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-d>f", builtin.find_files, { noremap = true, silent = true })
+      vim.keymap.set("n", "<C-d>b", builtin.buffers, { noremap = true, silent = true })
+      vim.keymap.set("n", "<C-d>l", builtin.live_grep, { noremap = true, silent = true })
+      vim.keymap.set("n", "<C-d>r", builtin.resume, { noremap = true, silent = true })
+      vim.keymap.set({ "n", "x" }, "<C-d>g", builtin.grep_string, { noremap = true, silent = true })
+      vim.keymap.set("n", "<C-f>b", telescope.extensions.file_browser.file_browser, { noremap = true, silent = true })
+    end,
+  },
+}

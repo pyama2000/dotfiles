b6a6edc471a92fe88645caf1e6b9f9569d16a8db
pyama2000 <8256tako@gmail.com>
2023-05-31 23:10:54 +0900

    remove: remove vim scripts

--- nvim/plugins/kanagawa.lua
+++
@@ -1,8 +0,0 @@
-require("kanagawa").setup({
-  overrides = function(colors)
-    return {
-      CocFloating = { fg = colors.palette.fugiWhite, bg = colors.palette.waveBlue1 },
-      CocMenuSel = { fg = colors.palette.fugiWhite, bg = colors.palette.waveBlue2, bold = true },
-    }
-  end,
-})
--- nvim/provider.vimrc
+++
@@ -1,2 +0,0 @@
-let g:python_host_prog = system('echo -n $(which python2)')
-let g:python3_host_prog = system('echo -n $(which python3)')

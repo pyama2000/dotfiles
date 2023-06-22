916ac9a2abf9d58d48f8e905ecfbedff9d0d9a70
pyama2000 <takahiko-yamashita@retty.me>
2023-06-23 01:40:08 +0900

    feat: add tfsec

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -9,6 +9,7 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
+        null_ls.builtins.diagnostics.tfsec,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.actionlint,
         null_ls.builtins.formatting.ruff,

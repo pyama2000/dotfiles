3d34301a95c0d6857ba79602fd681747dfcfa093
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2023-06-29 10:39:47 +0900

    nvim-cmp の設定を修正 (#240)
    * change: nvim-cmp.lua に依存関係と設定を移動
    
    * feat: path を source にする
    
    * feat: cmdline の補完をする
    
    * feat: load on `InsertEnter` / `CmdlineEnter`

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,6 +1,57 @@
 return {
   "hrsh7th/nvim-cmp",
+  event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp-document-symbol",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-path",
     { "ray-x/cmp-treesitter", dependencies = { "nvim-treesitter/nvim-treesitter" } },
+    { "hrsh7th/cmp-vsnip", dependencies = { "hrsh7th/vim-vsnip" } },
   },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          vim.fn["vsnip#anonymous"](args.body)
+        end,
+      },
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "path" },
+        { name = "vsnip" },
+      }, {
+        { name = "buffer" },
+        { name = "treesitter" },
+      }),
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+    })
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp_document_symbol" },
+        { name = "cmdline" },
+      }, {
+        { name = "buffer" },
+      }),
+      completion = {
+        completeopt = "menu,menuone,noselect",
+      },
+    })
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" }, { { name = "cmdline_history" } } }),
+      completion = {
+        completeopt = "menu,menuone,noselect",
+      },
+    })
+  end,
 }
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,14 +4,6 @@ return {
   -- FIXME: dependencies を整理する
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lsp-document-symbol",
-    "hrsh7th/cmp-nvim-lsp-signature-help",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/vim-vsnip",
-    "hrsh7th/cmp-vsnip",
     "b0o/schemastore.nvim",
   },
   config = function()

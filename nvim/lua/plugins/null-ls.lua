ae848933d24a478e9b2da4486e2566fdedf44cb1
pyama2000 <takahiko-yamashita@retty.me>
2023-06-29 09:52:40 +0900

    feat: builin な diagnostics / formatting を複数追加

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -4,13 +4,27 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local null_ls = require("null-ls")
+    local diagnostics = null_ls.builtins.diagnostics
+    local formatting = null_ls.builtins.formatting
+
     null_ls.setup({
       sources = {
-        null_ls.builtins.diagnostics.hadolint,
-        null_ls.builtins.diagnostics.tfsec,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.actionlint,
-        null_ls.builtins.formatting.ruff,
+        diagnostics.actionlint,
+        diagnostics.buf,
+        diagnostics.golangci_lint,
+        diagnostics.fish,
+        diagnostics.hadolint,
+        diagnostics.ruff,
+        diagnostics.terraform_validate,
+        diagnostics.tfsec,
+        diagnostics.typos,
+        diagnostics.yamllint,
+        formatting.buf,
+        formatting.fish_indent,
+        formatting.goimports,
+        formatting.shfmt,
+        formatting.stylua,
+        formatting.ruff,
       },
     })
   end,

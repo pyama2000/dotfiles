4f52d287bcaef86db6de74aa9a64aaf4221425cd
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2023-07-31 12:27:46 +0900

    change: fern のキーマップを <C-n> → <Space>f に変更 (#293)

--- nvim/lua/plugins/fern.lua
+++ nvim/lua/plugins/fern.lua
@@ -2,6 +2,6 @@ return {
   "lambdalisue/fern.vim",
   event = { "VeryLazy" },
   config = function()
-    vim.keymap.set("n", "<C-n>", "<cmd>Fern . -reveal=% -drawer -toggle<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<Space>f", "<cmd>Fern . -reveal=% -drawer -toggle<CR>", { noremap = true, silent = true })
   end,
 }

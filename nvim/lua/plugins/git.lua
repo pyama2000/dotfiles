e32a70800cde090fb2934bb87ffe1c6c9ba2c057
pyama2000 <takahiko-yamashita@retty.me>
2023-10-24 10:08:17 +0900

    chore: format

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
   "dinhhuy258/git.nvim",
   event = { "VeryLazy" },
-  config = function ()
-    require('git').setup()
-  end
+  config = function()
+    require("git").setup()
+  end,
 }

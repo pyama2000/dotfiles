9c521d83e1709ff0c181b778b8e15148c6290035
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2023-11-11 03:02:05 +0900

    change: use none-ls.nvim (#440)
    * change: use nvimtools/none-ls.nvim instead of null-ls.nvim
    
    * remove: commentout
    
    * remove: remove autoformat function

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,12 +1,11 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  "nvimtools/none-ls.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local null_ls = require("null-ls")
     local diagnostics = null_ls.builtins.diagnostics
     local formatting = null_ls.builtins.formatting
-    -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
     null_ls.setup({
       diagnostics_format = "[#{c}] #{m} (#{s})",
@@ -17,15 +16,15 @@ return {
         diagnostics.fish,
         diagnostics.hadolint,
         diagnostics.opacheck,
-        -- diagnostics.ruff,
-        -- diagnostics.sqlfluff.with({
-        --   extra_args = { "--dialect", "mysql" },
-        -- }),
+        diagnostics.ruff,
+        diagnostics.sqlfluff.with({
+          extra_args = { "--dialect", "mysql" },
+        }),
         diagnostics.terraform_validate,
         diagnostics.tfsec,
-        -- diagnostics.yamllint.with({
-        --   extra_args = { "--config-data", "{ extends: relaxed, rules: { line-length: { max: 120 } } }" },
-        -- }),
+        diagnostics.yamllint.with({
+          extra_args = { "--config-data", "{ extends: relaxed, rules: { line-length: { max: 120 } } }" },
+        }),
         formatting.buf,
         formatting.fish_indent,
         formatting.goimports,
@@ -37,29 +36,12 @@ return {
         formatting.shfmt.with({
           extra_args = { "--indent", "2", "--case-indent", "--binary-next-line", "--simplify" },
         }),
-        -- formatting.sqlfluff.with({
-        --   extra_args = { "--dialect", "mysql" },
-        -- }),
+        formatting.sqlfluff.with({
+          extra_args = { "--dialect", "mysql" },
+        }),
         formatting.stylua,
-        -- formatting.ruff,
+        formatting.ruff,
       },
-      -- on_attach = function(client, bufnr)
-      --   if client.supports_method("textDocument/formatting") then
-      --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      --     vim.api.nvim_create_autocmd("BufWritePre", {
-      --       group = augroup,
-      --       buffer = bufnr,
-      --       callback = function()
-      --         vim.lsp.buf.format({
-      --           bufnr = bufnr,
-      --           filter = function(c)
-      --             return c.name == "null-ls"
-      --           end,
-      --         })
-      --       end,
-      --     })
-      --   end
-      -- end,
     })
   end,
 }

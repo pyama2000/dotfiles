ab3208d85779060e795f335c63d86b75e10e5d3e
pyama2000 <takahiko-yamashita@retty.me>
2023-11-11 02:59:05 +0900

    remove: remove autoformat function

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -6,7 +6,6 @@ return {
     local null_ls = require("null-ls")
     local diagnostics = null_ls.builtins.diagnostics
     local formatting = null_ls.builtins.formatting
-    -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
     null_ls.setup({
       diagnostics_format = "[#{c}] #{m} (#{s})",
@@ -43,23 +42,6 @@ return {
         formatting.stylua,
         formatting.ruff,
       },
-      -- on_attach = function(client, bufnr)
-      --   if client.supports_method("textDocument/formatting") then
-      --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      --     vim.api.nvim_create_autocmd("BufWritePre", {
-      --       group = augroup,
-      --       buffer = bufnr,
-      --       callback = function()
-      --         vim.lsp.buf.format({
-      --           bufnr = bufnr,
-      --           filter = function(c)
-      --             return c.name == "null-ls"
-      --           end,
-      --         })
-      --       end,
-      --     })
-      --   end
-      -- end,
     })
   end,
 }

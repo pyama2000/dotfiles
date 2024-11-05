845679189f30585d026efe121930436c84769edb
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2024-11-05 10:56:07 +0900

    change: update Lazy.nvim (#1082)

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,22 +2,46 @@ require("options")
 require("keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
+-- if not vim.loop.fs_stat(lazypath) then
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable", -- latest stable release
+--     lazypath,
+--   })
+-- end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins", {
-  checker = {
-    enable = true,
+-- require("lazy").setup("plugins", {
+--   checker = {
+--     enable = true,
+--   },
+--   change_detection = {
+--     notify = false,
+--   },
+-- })
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
   },
-  change_detection = {
-    notify = false,
-  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
 })

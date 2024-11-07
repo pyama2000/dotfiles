54f8cea01c6b637772a45c839d172a9a4e7ed745
pyama2000 <takahiko-yamashita@retty.me>
2024-11-07 23:55:47 +0900

    add: add lualine.nvim

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,57 +1,55 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return {
-        options = {
-          icons_enabled = true,
-          theme = "auto",
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = {},
-          always_divide_middle = true,
-          globalstatus = false,
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = {
-            "branch",
-            {
-              "diagnostics",
-              sources = { "nvim_diagnostic" },
-              symbols = { error = " ", warn = " ", info = " ", hint = " " },
-              colored = true,
-            },
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function()
+    return {
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = false,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          "branch",
+          {
+            "diagnostics",
+            sources = { "nvim_diagnostic" },
+            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+            colored = true,
           },
-          lualine_c = { "filename" },
-          lualine_x = {
-            "g:coc_status",
-            "encoding",
-            "fileformat",
-            "filetype",
-          },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
         },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
+        lualine_c = { "filename" },
+        lualine_x = {
+          "g:coc_status",
+          "encoding",
+          "fileformat",
+          "filetype",
         },
-        tabline = {
-          lualine_a = { { "buffers", show_filename_only = false } },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { "tabs" },
-        },
-        extensions = {},
-      }
-    end,
-  },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {
+        lualine_a = { { "buffers", show_filename_only = false } },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "tabs" },
+      },
+      extensions = {},
+    }
+  end,
 }

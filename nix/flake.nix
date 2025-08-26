9151e5016a0767ae6886a23261bb924868361407
pyama2000 <takahiko-yamashita@retty.me>
2025-08-26 16:44:42 +0900

    change

--- nix/flake.nix
+++ nix/flake.nix
@@ -16,7 +16,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      homeConfigurations."default" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."${builtins.getEnv "USER"}" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         # Specify your home configuration modules here, for example,

c490837d710365a0a25b1e150d038d8b94333465
pyama2000 <takahiko-yamashita@retty.me>
2025-08-26 11:43:16 +0900

    feat: Home Managerを導入する

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,17 +1,30 @@
 {
-  description = "A very basic flake";
+  description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-    packages.aarch64-darwin.dotfiles = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
-        name = "packages";
-        paths = [
-            nixpkgs.legacyPackages.aarch64-darwin.eza
-            nixpkgs.legacyPackages.aarch64-darwin.git
-        ];
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = builtins.currentSystem;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."default" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
 }

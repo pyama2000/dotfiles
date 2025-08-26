5e8b6bc51bfe3787bea22fe8b1db0c49856ccb08
pyama2000 <takahiko-yamashita@retty.me>
2025-08-26 11:38:15 +0900

    chore: `eza` / `git` コマンドをインストールする

--- nix/flake.nix
+++ nix/flake.nix
@@ -6,10 +6,12 @@
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
-
-    packages.aarch64-darwin.default = self.packages.aarch64-darwin.hello;
-
+    packages.aarch64-darwin.dotfiles = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
+        name = "packages";
+        paths = [
+            nixpkgs.legacyPackages.aarch64-darwin.eza
+            nixpkgs.legacyPackages.aarch64-darwin.git
+        ];
+    };
   };
 }

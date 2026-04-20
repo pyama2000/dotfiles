5e0114120849f5b7287ea5d2bf04c3a906b1cf29
pyama2000 <takahiko-yamashita@retty.me>
2026-04-20 17:16:41 +0900

    fix: TerraformのプロジェクトをNeovimで開くとフリーズするので修正する

--- nvim/after/lsp/terraformls.lua
+++ nvim/after/lsp/terraformls.lua
@@ -1,4 +1,13 @@
 ---@type vim.lsp.Config
 return {
   filetypes = { "terraform", "terraform-vars", "tf" },
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local root = vim.fs.root(bufnr, ".terraform")
+    on_dir(root or vim.fs.dirname(fname))
+  end,
+  on_attach = function(client, _bufnr)
+    -- terraform-ls の semantic tokens は大きなファイルで Neovim をフリーズさせるため無効化
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
 }
--- nvim/after/lsp/tflint.lua
+++ nvim/after/lsp/tflint.lua
@@ -1,4 +1,9 @@
 ---@type vim.lsp.Config
 return {
   filetypes = { "terraform", "terraform-vars", "tf" },
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local root = vim.fs.root(bufnr, ".terraform")
+    on_dir(root or vim.fs.dirname(fname))
+  end,
 }
